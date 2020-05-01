1ab8700e9373192370fd86c3527241501c3c5dde
Jacanroman <jacanroman@gmail.com>
2020-05-01 12:49:01 +0100

    last changes

--- handler.js
+++ handler.js
@@ -2,14 +2,14 @@
 const serverlessHttp = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");  // para leer los json con serverless
+const bodyParser = require("body-parser");  
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  database:"Tasks"
+  password: process.env.DB_PASSWORD,
+  database: "Tasks"
 });
 
 // Logically separate 4 sections of code according to the method of the HTTP request recieved
@@ -21,16 +21,13 @@ app.use(bodyParser.json());
 
 
 
-app.get("/tasks", function(request, response){
-  //Should make a SELECT * FROM Tasks query to the DB and return the result
-  //Request has loads of info about the request
-  // Response has some useful methods for sending a response
-
-  connection.query("SELECT * FROM Task", function(err,data){
-    if(err){
-      console.log("Error from MySQL",err);
+app.get("/tasks", function (request, response) {
+  
+  connection.query("SELECT * FROM Task ORDER BY DueDate", function (err, data) {
+    if (err) {
+      console.log("Error from MySQL", err);
       response.status(500).send(err);
-    }else{
+    } else {
       response.status(200).send(data)
     }
   });
@@ -39,53 +36,50 @@ app.get("/tasks", function(request, response){
 
 
 
-app.delete("/tasks/:id", function(request, response){
-  
+app.delete("/tasks/:id", function (request, response) {
+
   const id = request.params.id;
   const query = `DELETE FROM Task WHERE TaskId = ?`;
 
-  connection.query(query, [id], function(err){
-    if(err){
-      console.log("Error from MySQL",err);
+  connection.query(query, [id], function (err) {
+    if (err) {
+      console.log("Error from MySQL", err);
       response.status(500).send(err);
-    }else{
+    } else {
       response.status(200).send(`Deleted task with ID: ${id}`)
     }
   });
-  
+
   // Should delete the task with the specified ID from the database
   //response.status(200).send(`Deleted task with ID ${id}!`);
 });
 
 
 
-app.post("/tasks", function(request, response){
-  
-  const data = request.body; 
+app.post("/tasks", function (request, response) {
+
+  const data = request.body;
 
   //SQL INJECTION PROBLEM
   /*const query = `INSERT INTO Task (Description, Completed, DueDate) VALUES ("${data.Description}", FALSE, "${data.DueDate}")`;
   NO USAMOS EL QUERY ANTERIOR PORQUE ES MUY INSEGURO*/
   //to avoid the SQL injection we "escaping" user-provided values
 
-  //const query = `INSERT INTO Task (Description, Completed, DueDate) VALUES(?,?,?)`;
-
   const query = `INSERT INTO Task (Description, Completed, DueDate, UserId) VALUES(?,?,?,?)`;
 
 
-  connection.query(query,[data.Description,false, data.DueDate, data.UserId],function(err,results){
-  //connection.query(query,[data.Description,false, data.DueDate],function(err,results){
-    if(err){
-      console.log("Error from MySQL",err);
+  connection.query(query, [data.Description, false, data.DueDate, data.UserId], function (err, results) {
+    if (err) {
+      console.log("Error from MySQL", err);
       response.status(500).send(err);
-    } else{
+    } else {
       // Send back the newly created task
       // Because the frontend(client) migh want to know the ID
-      connection.query(`SELECT * FROM Task WHERE TaskId = ${results.insertId}`, function(err,results){
-        if(err){
-          console.log("Error from MySQL",err);
+      connection.query(`SELECT * FROM Task WHERE TaskId = ${results.insertId} `, function (err, results) {
+        if (err) {
+          console.log("Error from MySQL", err);
           response.status(500).send(err);
-        }else{
+        } else {
           response.status(201).send(results[0]);
         }
 
@@ -93,32 +87,29 @@ app.post("/tasks", function(request, response){
     }
 
   });
-  
+
   //response.status(201).send(`New task of ${data.text} created`);
 });
 
 
 
-app.put("/tasks/:id", function(request, response){
-  
+app.put("/tasks/:id", function (request, response) {
+
   const data = request.body;
   const id = request.params.id;
 
-  const query = "UPDATE Task SET Description = ?, Completed = ?, DueDate = ?, UserId = ? WHERE TaskId = ?";
+  const query = "UPDATE Task SET ? WHERE TaskId = ?";
 
-  connection.query(query, [data.Description, data.Completed, data.DueDate, data.UserId, id],function(err){
-    if(err){
-      console.log("Error from MySQL",err);
+  connection.query(query, [data, id], function (err) {
+    if (err) {
+      console.log("Error from MySQL", err);
       response.status(500).send(err);
-    }else{
+    } else {
       response.status(205).send(`Updated task with ID: ${id}`);
       //response.status(205).json({product: data});
     }
   })
-  //should Update a task in the 
-  // For now, just send back a text message
-  //response.status(200).send(`Update task with ID ${id}! and data ${JSON.stringify(data)}`);
-
+  
 });
 
 
